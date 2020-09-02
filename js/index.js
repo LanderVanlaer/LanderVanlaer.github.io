f26b1888c69cc9c3a584a30c3a39bd81236d3180
Lander Van laer <lander.vanlaer1@gmail.com>
2020-09-02 21:57:25 +0200

    Begin of new design
    Making begin

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
-window.onload = () => {
-    document.querySelectorAll('section.repos').forEach(sec => {
-        if (sec.dataset.json) loadJson(sec.querySelector('ul'), `/${sec.dataset.json}.json`)
-    })
-}
+// @ts-nocheck
+window.addEventListener('scroll', e => {
+    console.log(e);
+});
 
-const loadJson = async (ul, fetchLink) => {
-    try {
-        const response = await fetch(fetchLink);
-        if (!(response.status >= 200 && response.status < 400)) throw new Error(`Got response ${response.status} instead of 200`);
-        const data = await response.json();
-        data.forEach(repo => {
-            ul.innerHTML += `<li><a href="${repo.link}">${repo.data.name}</a></li>`;
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
+const image = document.querySelector('img#wave');
+const bannerImage = () => {
+    const height = image.getBoundingClientRect().height - 800;
+    image.style.top = `${height > 0 ? -height : 0}px`;
+};
+
+window.addEventListener('load', bannerImage);
+window.addEventListener('resize', bannerImage);
