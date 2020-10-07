21f4a3ea2ef393dc9ae9e9cca7d2e58a3385c12a
Lander Van laer <lander.vanlaer1@gmail.com>
2020-10-07 16:30:02 +0200

    Remove everything

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-window.onload = () => {
-    document.querySelectorAll('section.repos').forEach(sec => {
-        if (sec.dataset.json) loadJson(sec.querySelector('ul'), `/${sec.dataset.json}.json`)
-    })
-}
-
-const loadJson = async (ul, fetchLink) => {
-    try {
-        const response = await fetch(fetchLink);
-        if (!(response.status >= 200 && response.status < 400)) throw new Error(`Got response ${response.status} instead of 200`);
-        const data = await response.json();
-        data.forEach(repo => {
-            ul.innerHTML += `<li><a href="${repo.link}">${repo.data.name}</a></li>`;
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
+location.replace("https://github.com/LanderVanlaer");
